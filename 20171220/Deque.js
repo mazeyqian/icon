c6da885203dc4e13b9bc1fd96fd2ceb7a947cdd4
mazey <mazey@mazey.net>
2017-12-24 18:37:37 +0800

    队列

--- 20171220/Deque.js
+++ 20171220/Deque.js
@@ -1,20 +1,30 @@
-// 队列类
-function Queue () {
+// 双向队列类
+function Deque () {
     this.dataStore = [];
-    this.enqueue = enqueue;
-    this.dequeue = dequeue;
+    this.enqueueFront = enqueueFront;
+    this.enqueueBack = enqueueBack;
+    this.dequeueFront = dequeueFront;
+    this.dequeueBack = dequeueBack;
     this.front = front;
     this.back = back;
     this.toString = toString;
     this.empty = empty;
 }
-// 入队
-function enqueue (element) {
+// 入队 - 队首
+function enqueueFront (element) {
+    this.dataStore.unshift(element);
+}
+// 出队 - 队首
+function dequeueFront () {
+    return this.dataStore.shift();
+}
+// 入队 - 队尾
+function enqueueBack (element) {
     this.dataStore.push(element);
 }
-// 出队
-function dequeue () {
-    return this.dataStore.shift();
+// 出队 - 队尾
+function dequeueBack (element) {
+    return this.dataStore.pop(element);
 }
 // 读取队首的元素
 function front () {
