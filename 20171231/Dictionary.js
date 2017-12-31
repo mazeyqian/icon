f29cedf567556ef1b822acd84cc7bde0fa9cdc53
mazey <mazey@mazey.net>
2017-12-31 16:25:04 +0800

    准备添加排序功能

--- 20171231/Dictionary.js
+++ 20171231/Dictionary.js
@@ -4,7 +4,35 @@ function Dictionary () {
     this.find = find;
     this.remove = remove;
     this.showAll = showAll;
+    this.count = count;
+    this.clear = clear;
 }
 function add (key, value) {
     this.dataStore[key] = value;
+}
+function find (key) {
+    return this.dataStore[key];
+}
+function remove (key) {
+    delete this.dataStore[key];
+}
+function showAll () {
+    let datakeys = Array.prototype.slice.call(Object.keys(this.dataStore));
+    for (let key in datakeys) {
+        console.log(`${datakeys[key]} -> ${this.dataStore[datakeys[key]]}`);
+    }
+}
+// 计数
+function count () {
+    let n = 0;
+    for (let key in Object.keys(this.dataStore)) {
+        ++n;
+    }
+    return n;
+}
+// 清空
+function clear () {
+    Object.keys(this.dataStore).forEach((key) => {
+        delete this.dataStore[key];
+    }, this);
 }
